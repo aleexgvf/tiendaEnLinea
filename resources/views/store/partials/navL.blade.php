40762133aa0eb540fedd3e52baa832a1ad9e5f7b
michael <chael35@hotmail.com>
2017-07-31 19:20:54 -0600

    Evidencias

--- resources/views/store/partials/navL.blade.php
+++ resources/views/store/partials/navL.blade.php
@@ -19,6 +19,9 @@
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-user"></i> <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="{{url('/perfiles/profile')}}">Cambiar foto</a></li>
+            <li>Buenos dias </li>
+            
+            
 
           
 
